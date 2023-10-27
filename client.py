8b6f50e666120a702dd2e721ee6de0efa7f744fa
victor-POL <correovictorp@gmail.com>
2023-10-26 23:03:55 -0300

    Refactor draw stickman

--- client.py
+++ client.py
@@ -9,43 +9,27 @@ port = 12345
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+stick_man_parts = [
+    (4, 2, "|"),
+    (5, 2, "|"),
+    (6, 2, "O"),
+    (7, 1, "/"),
+    (7, 2, "|"),
+    (7, 3, "\\"),
+    (8, 2, "|"),
+    (9, 2, "/"),
+    (9, 3, "\\"),
+]
 
 def signal_handler(sig, frame):
     print('Close client')
     client_socket.close()
     sys.exit(0)
 
-
 def draw_stick_man(stdscr, attempts):
-    if attempts == 9:
-        stdscr.addstr(4, 2, "|")
-
-    if attempts == 8:
-        stdscr.addstr(5, 2, "|")
-
-    if attempts == 7:
-        stdscr.addstr(6, 2, "O")
-
-    if attempts == 6:
-        stdscr.addstr(7, 1, "/")
-
-    if attempts == 5:
-        stdscr.addstr(7, 2, "|")
-
-    if attempts == 4:
-        stdscr.addstr(7, 3, "\\")
-
-    if attempts == 3:
-        stdscr.addstr(8, 2, "|")
-
-    if attempts == 2:
-        stdscr.addstr(9, 2, "/")
-
-    if attempts == 1:
-        stdscr.addstr(9, 3, "\\")
-
-    stdscr.refresh()
-
+    for i in range(attempts):
+        y, x, char = stick_man_parts[i]
+        stdscr.addstr(y, x, char)
 
 def main(stdscr):
     signal.signal(signal.SIGINT, signal_handler)
@@ -57,7 +41,7 @@ def main(stdscr):
     stdscr.addstr(1, 0, "Input letter: ")
     stdscr.refresh()
 
-    attempts = 9
+    attempts = 1  # Inicializado a 1
     while True:
         guess = chr(stdscr.getch()).lower()
 
@@ -66,33 +50,22 @@ def main(stdscr):
         response = client_socket.recv(1024).decode()
 
         if "Invalid" in response or "Wrong" in response:
-            if "Wrong" in response:
-                draw_stick_man(stdscr, attempts)
+            draw_stick_man(stdscr, attempts)
             stdscr.move(3, 0)
             stdscr.clrtoeol()
             stdscr.addstr(response)
             stdscr.refresh()
-            attempts -= 1
+            attempts += 1
         else:
             stdscr.move(2, 0)
             stdscr.clrtoeol()
             stdscr.addstr(response)
             stdscr.refresh()
 
-        if "lose" in response:
+        if "lose" in response or "win" in response:
             stdscr.move(0, 0)
             stdscr.clrtobot()
             stdscr.addstr(response)
-            stdscr.move(1, 0)
-            stdscr.addstr("Press any key to exit.")
-            stdscr.refresh()
-            stdscr.getch()
-            break
-
-        if "win" in response:
-            stdscr.move(0, 0)
-            stdscr.clrtobot()
-            stdscr.addstr("Congratulations! You won!")
             stdscr.move(1, 0)
             stdscr.addstr("Press any key to exit.")
             stdscr.refresh()
@@ -100,7 +73,6 @@ def main(stdscr):
             break
 
     client_socket.close()
-
 
 if __name__ == "__main__":
     wrapper(main)
